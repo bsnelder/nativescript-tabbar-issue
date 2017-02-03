e5fe49fdc897a874ad85f9a5fa5a07b213e88cad
Bastiaan Snelder <bastiaan@elements.nl>
2017-02-03 09:02:52 +0100

    Initial commit

---
+++ app/blank/blank.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "blank-page",
+    moduleId: module.id,
+    templateUrl: "./blank.component.html"
+})
+export class BlankComponent {}
